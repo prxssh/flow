24cd8075f9a28ce6fcce1c8f7829174fc6ce3ab6
prxssh <prxssh@pm.me>
2025-07-16 00:10:12 +0530

    chore: add Dockerfile

--- backend/mix.exs
+++ backend/mix.exs
@@ -4,8 +4,8 @@ defmodule Backend.MixProject do
   def project do
     [
       app: :backend,
-      version: "0.1.0",
-      elixir: "~> 1.14",
+      version: "1.0.0",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
